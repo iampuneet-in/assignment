8081d1395a51eb05090bb3d8fd5caa32441e9401
Puneet Sharma <puneet.sharma@maxinai.com>
2022-08-27 16:55:04 +0530

    feat: :triangular_flag_on_post: adding dynamic data in genre slider

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,53 +1,32 @@
 <template>
  <section class="mt-16">
-   <carousel :items-to-show="5" :wrap-around="true">
-     <slide v-for="slide in 10" :key="slide">
-        <card />
-    </slide>
-    <template #addons>
-      <navigation />
-    </template>
-   </carousel>
+   <genreSlider v-for="(shows, index) in showsByGenre"   :key="`slider-${index}`"  :shows="shows.shows"   :genre="shows.genre">
+   </genreSlider>
  </section>
 </template>
 
 <script>
 import { useStore } from 'vuex'
 import { ref, onMounted } from 'vue'
-import { Carousel, Navigation, Slide } from "vue3-carousel";
-import Card from '../components/Card.vue'
+import GenreSlider from '@/components/GenreSlider.vue'
+import { useGroupByGenre } from '../helpers/composables';
 
 import "vue3-carousel/dist/carousel.css";
 export default {
-  components: { Carousel, Navigation, Slide, Card },
+  components: { GenreSlider },
   setup() {
     const store = useStore()
-    const shows = ref([])
+    const showsByGenre = ref({})
     onMounted(async () => { 
       const response = await store.dispatch('get', {
         action: '/shows'
       });
-      shows.value = response
+      showsByGenre.value = useGroupByGenre(response);
     })
+
+    return {
+      showsByGenre
+    }
   }
 }
 </script>
-
-<style scoped>
-.carousel__slide {
-  padding: 10px;
-}
-
-.carousel__item {
-  color: white;
-  font-size: 20px;
-  border-radius: 8px;
-  display: flex;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-  border: 5px solid white;
-}
-</style>
